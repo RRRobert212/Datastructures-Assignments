fa625a47fbecae52c849bef303d0b79520101d85
RRRobert212 <robertwd1@gmail.com>
2024-05-06 20:17:39 -0400

    DFS on adjaceny list

---
+++ adjacencyListDFS.cpp
@@ -0,0 +1,35 @@
+//adjacenyListDFS.cpp
+
+
+//Implement a C++ function that uses DFS to check if a graph represented as an adjacency list contains a cycle. Please include your .cpp file in your submission. 
+
+
+#include <stddef.h>
+#include<bits/stdc++.h>
+using namespace std;
+            
+int main(){
+
+    //rather than building a whole adjaceny list structure, we just make our own using a vector of vectors.
+    vector<vector<int>> adjList;
+    adjList = {
+            {1,2},
+            {2,3},
+            {0,1},
+            {1,4},
+            {3}
+        };
+
+
+
+
+            
+            
+            
+   return 0;
+            
+}
+
+
+
+
